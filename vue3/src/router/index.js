619434166c9120e6ebe0bbbd0e69e631c72b02a6
597 <wuj18595478953@163.com>
2026-04-17 16:49:14 +0800

    [init] 初始化项目

---
+++ vue3/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AppLayout from '../layouts/AppLayout.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: AppLayout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import('../views/HomeView.vue'),
+        },
+        {
+          path: 'about',
+          name: 'about',
+          component: () => import('../views/AboutView.vue'),
+        },
+      ],
+    },
+  ],
+})
+
+export default router
