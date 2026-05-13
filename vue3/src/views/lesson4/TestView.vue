80785e5241e05e77d3994022bfbe897004d8efa8
597 <wuj18595478953@163.com>
2026-05-13 17:11:17 +0800

    [fix] 新增toRefs, toRef用法

--- vue3/src/views/lesson4/TestView.vue
+++ vue3/src/views/lesson4/TestView.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="test-content">实力</div>
+  <div>姓名: {{ name }}</div>
+  <div>年龄: {{ age }}</div>
+  <br />
+  <el-button type="Primary" @click="changeName">修改姓名</el-button>
+  <el-button type="Primary" @click="changeAge">年龄++</el-button>
 </template>
 
 <script setup>
+// toRefs: 它就是将一个响应式的对象的所有属性，转换为单独的响应式的引用，通常用于结构赋值，获取数据需要.value
+// toRef:  创建单个属性的引用，源对象发生改变，新对象会同步更新
+import { reactive, toRefs, toRef } from 'vue'
+let person = reactive({
+  name: 'foo',
+  age: 18,
+})
+// let { name, age } = person; 结构失去响应式
+let { name, age } = toRefs(person)
+const newName = toRef(person, 'name')
+
+const changeName = () => {
+  name.value += 'o'
+  console.log(name.value, 'newName', newName.value)
+}
+const changeAge = () => {
+  age.value += 1
+  console.log(age.value)
+}
 </script>
 
 <style scoped>
-.test-content {
+div {
   padding: 0;
   color: inherit;
   font-size: 28px;
